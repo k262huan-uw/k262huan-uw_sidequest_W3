b9dcf8526c6e26a17ef42ac28280c6a70c8daaf7
k262huan-uw <k262huan@uwaterloo.ca>
2026-01-29 21:28:49 -0500

    testing random width again

--- game.js
+++ game.js
@@ -15,7 +15,7 @@
 const gameBtn = {
   x: 400, // x position (centre of the button)
   y: 550, // y position (centre of the button)
-  w: 260, // width
+  w: random(260), // width
   h: 90, // height
   label: "PRESS HERE", // text shown on the button
 };
