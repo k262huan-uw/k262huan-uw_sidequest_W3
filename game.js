88b34d1acd859d03d2132f44963ff9f1a0679f81
k262huan-uw <k262huan@uwaterloo.ca>
2026-01-29 22:29:32 -0500

    lighter colour

--- game.js
+++ game.js
@@ -73,7 +73,7 @@ function drawGameButton({ x, y, w, h, label }) {
     red(randomBg * 0.5),
     green(randomBg * 0.5),
     blue(randomBg * 0.5),
-    220,
+    115,
   );
   // This gives visual feedback to the player
   fill(buttonCol);
