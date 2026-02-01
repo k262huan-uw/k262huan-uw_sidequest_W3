74fc7a21f16e95d0e61ae0d99dcc200afd4a4205
k262huan-uw <k262huan@uwaterloo.ca>
2026-01-31 20:56:56 -0500

    Update hard.js

--- hard.js
+++ hard.js
@@ -11,10 +11,6 @@ function drawHard() {
   text("Click on the correct crown to find...", width / 2, 210);
 
   image(crownImg, 200, 280, 50, 50); // click to win
-  image(crownImg, 400, 500, 100, 100);
-  image(crownImg, 350, 330, 300, 300);
-  image(crownImg, 500, 400, 505, 505);
-  image(crownImg, -20, 100, 700, 700);
 }
 
 function hardMousePressed() {
