a42d5ad9068376f7777d7f80bdb2421cedc37c7a
k262huan-uw <k262huan@uwaterloo.ca>
2026-01-31 19:05:00 -0500

    updating crown parameters

--- hard.js
+++ hard.js
@@ -14,7 +14,7 @@ function drawHard() {
   image(crownImg, 400, 500, 100, 100);
   image(crownImg, 350, 330, 300, 300);
   image(crownImg, 500, 400, 505, 505);
-  image(crownImg, 100, 100, 700, 700);
+  image(crownImg, 20, 100, 700, 700);
 }
 
 function hardMousePressed() {
