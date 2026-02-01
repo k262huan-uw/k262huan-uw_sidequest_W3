cc39caf1cbee064548d2a7afb6abbc3245101532
k262huan-uw <k262huan@uwaterloo.ca>
2026-01-31 19:00:41 -0500

    change img size

--- hard.js
+++ hard.js
@@ -10,9 +10,9 @@ function drawHard() {
   textSize(18);
   text("Click on the correct crown to find...", width / 2, 210);
 
-  image(crownImg, 200, 280, 50, 50);
-  image(crownImg, 700, 500, 100, 100);
-  image(crownImg, 350, 330, 50, 50);
+  image(crownImg, 200, 280, 50, 50); // click to win
+  image(crownImg, 600, 500, 100, 100);
+  image(crownImg, 350, 330, 300, 300);
   image(crownImg, 550, 700, 75, 75);
   image(crownImg, 100, 750, 120, 120);
 }
